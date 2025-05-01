ac7cf76982692d8a9d55e070daa6bf8cbce06c71
Sedose <Sedose>
2025-05-01 17:33:41 +0300

    Revert "Refactor two-character token parsing logic"
    This reverts commit f91deae5730cfb19c88214e7c55fb500809f17fb.

--- src/interpreter/rule/two_character_rule.rs
+++ src/interpreter/rule/two_character_rule.rs
@@ -8,27 +8,13 @@ pub fn two_character_rule(
     character_iterator: &mut Peekable<Chars>,
     _line_number: &mut usize,
 ) -> Option<Token> {
-    let first_character = match character_iterator.peek() {
-        Some(character) => *character,
-        None => return None,
-    };
-
-    let mut lookahead_iterator = character_iterator.clone();
-    lookahead_iterator.next();
-
-    let second_character = match lookahead_iterator.peek() {
-        Some(character) => *character,
-        None => return None,
-    };
-
-    let (token_type, lexeme) = match two_character_token(first_character, Some(&second_character)) {
-        Some(token) => token,
-        None => return None,
-    };
-
+    let current_character = *character_iterator.peek()?;
+    let mut ahead_iterator = character_iterator.clone();
+    ahead_iterator.next();
+    let next_character = *ahead_iterator.peek()?;
+    let (token_type, lexeme) = two_character_token(current_character, Some(&next_character))?;
     character_iterator.next();
     character_iterator.next();
-
     Some(Token::new(token_type, lexeme))
 }
 
